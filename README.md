ab9c9f7dcf39b03168b2a13bc1cea25e3eba98af
Maggi Chen <38887640+Maggi-Chen@users.noreply.github.com>
2021-04-25 11:36:51 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,128 @@
+# DeBreak
+
+A reference-free assembly evaluator.
+
+Author: Maggi Chen
+
+Email: maggic@uab.edu
+
+Draft date: Apr. 20, 2021
+
+## Quick Start
+```sh
+git clone https://github.com/Maggi-Chen/Inspector.git
+cd Inspector/
+./inspector.py -h
+
+# Evaluate assembly with raw reads
+./inspector.py -c contig.fa -r rawreads.1.fastq rawreads.2.fastq -o inspector_out/ --datatype clr 
+# Evaluate assembly with hifi reads
+./inspector.py -c contig.fa -r ccsreads.1.fastq ccsreads.2.fastq -o inspector_out/ --datatype hifi
+
+# With reference-based evaluation
+./inspector.py -c contig.fa -r rawreads.1.fastq --ref reference.fa -o inspector_out/ --datatype clr
+
+# Reference-based only evaluation
+./inspector.py -c contig.fa -r emptyfile --ref reference.fa -o inspector_out/ 
+
+# Error correction
+
+
+```
+
+
+
+## Description
+
+DeBreak is a tool for assembly evaluation with long read data. The input includes a contig file, long reads (PacBio CLR, PacBio HiFi, Oxford Nanopore, or mixed platform), and a reference genome (optional). The output includes A summary report, read-to-contig alignment file, a list of structrual errors and small-scale errors. This program was tested on a x86_64 Linux system with a 8GB physical memory.
+
+## Depencency
+
+Dependencies for Inspector:
+python 2.7  
+python package:  pysam, statsmodels  
+
+* minimap2  (tested with version 2.10 and 2.15)
+* samtools  (tested with version 1.9)
+
+Dependencies for Inspector error correction module:
+* flye  (tested with version 2.8.2)
+
+
+## Installation
+
+```
+git clone https://github.com/Maggi-Chen/Inspector.git
+```
+Then, please also add this directory to your PATH:
+```
+export PATH=$PWD/DeBreak/:$PATH
+```
+
+## General usage
+
+
+```
+
+inspector.py [-h] -c contig.fa -r raw_reads.fa -o output_dict/
+  required arguments:
+  --contig,-c           FASTA/FASTQ file containing contig sequences to be evaluated
+  --read,-r             A list of FASTA/FASTQ files containing long read sequences
+
+  optional arguments:
+  -h, --help            Show this help message and exit
+  --version             Show program's version number and exit
+  --datatype,-d         Input read type. (clr, hifi, nanopore, mixed) [clr]
+  --ref                 OPTIONAL reference genome in .fa format
+  --thread,-t           Number of threads. [8]
+  --min_contig_length   Minimal length for a contig to be evaluated [10000]
+  --min_contig_length_assemblyerror    Minimal contig length for assembly error detection. [1000000]
+  --pvalue              Maximal p-value for small-scale error identification [0.01 for HiFi, 0.05 for others]
+  --skip_read_mapping   Skip the step of mapping reads to contig
+  --skip_structural_error       Skip the step of identifying large structural errors
+  --skip_structural_error_detect       Skip the step of detecting large structural errors
+  --skip_base_error     Skip the step of identifying small-scale errors
+  --skip_base_error_detect      Skip the step of detecting small-scale errors from pileup
+
+
+inspector-correct.py [-h] -i inspector_out/ --data pacbio-raw 
+  required arguments:
+  --inspector,-i        Inspector evaluation directory with original file names
+  --data                Type of read used for Inspector evaluation. Required for structural error correction
+  --outpath,-o          Output directory
+  --thread,-t           Number of threads
+  --skip_structural     Do not correct structural errors. Local assembly will not be performed
+  --skip_baseerror      Do not correct small-scale errors
+  
+
+```
+
+## Use cases
+Inspector evaluates the contigs and identifies assembly errors with sequencing reads. You can use reads from single platform:
+```
+./inspector.py -c contig.fa -r rawreads.1.fastq rawreads.2.fastq -o inspector_out/ --datatype clr
+```
+Or use a mixed data type:
+```
+./inspector.py -c contig.fa -r rawreads.fastq nanopore.fastq -o inspector_out/ --datatype mixed
+```
+Reference-based evaluation is also supported:
+(Note that reported assembly error from reference-based mode will contain genetic variants)
+```
+./inspector.py -c contig.fa -r rawreads.fastq --ref reference.fa -o inspector_out/ --datatype clr
+```
+If only the continuity analysis is needed, simply provide an empty file for --read:
+```
+./inspector.py -c contig.fa -r emptyfile -o inspector_out/ --skip_base_error --skip_structural_error
+```
+For the '--skip' options, do not use unless you are repeating the evaluation with same contig and read files in the same output directory. These may help save time when testing different options for error detection.
+
+
+Inspector provides an error-correction module to improve assembly accuracy. High-accuracy reads are recommended, especially for small-scale error correction:
+(Note that only reads from single platform are supported for error correction.)
+```
+./inspector-correct.py -i inspector_out/ --datatype hifi -o inspector_out/ 
+```
+
+
+
---
+++ debreak_merge_clustering.py
@@ -0,0 +1,506 @@
+import os
+import time
+import sys
+import pysam
+
+def mergeinfolengthsort(a):
+        return int(a.split('\t')[2])
+
+def mergeinfo_insertion(candi,min_support):
+        candi.sort(key=mergeinfolengthsort)
+
+        if len(candi)>=1.5*min_support:
+                upper=int(candi[len(candi)*3/4].split('\t')[2])
+                lower=int(candi[len(candi)/4].split('\t')[2])
+                if upper>1.75*lower and upper-lower>50:
+                        svgroups=assign_candi_insertion(candi,upper,lower)
+                        svgroups=assign_candi_insertion(candi,svgroups[2],svgroups[3])
+                        svgroups=assign_candi_insertion(candi,svgroups[2],svgroups[3])
+                        mergedsv=[]
+                        if len(svgroups[0])>=min_support:
+                                mergedsv+=mergeinfo_insertion_oneevent(svgroups[0],min_support)
+                        if len(svgroups[1])>=min_support:
+                                mergedsv+=mergeinfo_insertion_oneevent(svgroups[1],min_support)
+                        if len(mergedsv)==2:
+                                mergedsv=[c+'\tCompoundSV' for c in mergedsv]
+                        if len(mergedsv)==1:
+                                mergedsv=[mergedsv[0]+'\tUnique']
+                        return mergedsv
+        mergedsv=mergeinfo_insertion_oneevent(candi,min_support)
+        if len(mergedsv)==1:
+                return [mergedsv[0]+'\tUnique']
+        else:
+                return []
+
+def assign_candi_insertion(candi,mean1,mean2):
+        group1=[]
+        group2=[]
+        for c in candi:
+                if abs(int(c.split('\t')[2])-mean1)<=abs(mean2-int(c.split('\t')[2])):
+                        group1+=[c]
+                else:
+                        group2+=[c]
+        mean1_new=sum([int(c.split('\t')[2]) for c in group1])/len(group1)
+        mean2_new=sum([int(c.split('\t')[2]) for c in group2])/len(group2)
+        return [group1,group2,mean1_new,mean2_new]
+
+
+def mergeinfo_insertion_oneevent(candi,min_support):
+        candi.sort(key=mergeinfolengthsort)
+	min_support=max(2,min_support)
+        while len(candi)>max(2,min_support-2):
+                if int(candi[-1].split('\t')[2]) > 2* int(candi[len(candi)/2].split('\t')[2]) and  int(candi[-1].split('\t')[2]) -int(candi[len(candi)/2].split('\t')[2]) >30:
+                        candi.remove(candi[-1])
+                        continue
+                if int(candi[len(candi)/2].split('\t')[2]) >  2*int(candi[0].split('\t')[2]) and int(candi[len(candi)/2].split('\t')[2]) -int(candi[0].split('\t')[2]) >30:
+                        candi.remove(candi[0])
+                        continue
+                break
+        if len(candi)>=max(2,min_support):
+                chrom=candi[0].split('\t')[0]
+                position=[int(c.split('\t')[1]) for c in candi]
+                length=[int(c.split('\t')[2]) for c in candi]
+                quality=[float(c.split('\t')[6]) for c in candi]
+                position=sum(position)/len(position)
+                quality=sum(quality)/float(len(quality))
+                length=sum(length)/len(length)
+                readnames=''
+                for c in candi:
+                        readnames+=c.split('\t')[4]+';'
+                readnames=readnames[:-1]
+		numread=len(readnames.split(';'))
+                return[chrom+'\t'+str(position)+'\t'+str(length)+'\t'+str(len(candi))+'\t'+str(numread)+'\t'+str(quality)+'\t'+'\t'+readnames]
+        else:
+                return []
+
+def counttimesort(a):
+        return [int(a.split('\t')[1]),int(a.split('\t')[2])]
+
+def cluster(outpath,chrom,contiglength,mins,maxdepth):
+	t1=time.time()
+	#allsv=open(outpath+'read_to_contig_'+chrom+'.debreak.temp','r').read().split('\n')[:-1]
+	allsv=open(outpath+'debreak_workspace/read_to_contig_'+chrom+'.debreak.temp','r').read().split('\n')[:-1]
+
+	# Large DEL
+	largesv=[c for c in allsv if 'D-' in c and int(c.split('\t')[2])>2000]
+	window=1600
+	largesv.sort(key=counttimesort)
+        largedel=[]
+	#try:
+        #start=int(largesv[0].split('\t')[1])
+	start=0
+        candi=[]
+        for event in largesv:
+                if int(event.split('\t')[1])<=start+window:
+                        candi+=[event]
+                        continue
+                if len(candi)>=mins:
+                        largedel+=mergeinfo_insertion(candi,mins)
+                candi=[event]
+                start=int(event.split('\t')[1])
+        if len(candi)>=mins:
+                largedel+=mergeinfo_insertion(candi,mins)
+                candi=[]
+
+	#smaller DEL
+	allsv=[c for c in allsv if 'D-' in c and int(c.split('\t')[2])<=3000]
+
+	print len(allsv)
+
+	genomeposition=[0]*contiglength
+
+	for c in allsv:
+		start=int(c.split('\t')[1])
+		end=int(c.split('\t')[1])+int(c.split('\t')[2])
+		original=genomeposition[start-1:end-1]
+		new=[mm+1 for mm in original]
+		genomeposition[start-1:end-1]=new
+	svregion=[]
+	inblock=False
+	threshold=3
+
+	for i in range(len(genomeposition)):
+		if inblock:
+			if genomeposition[i]>=max(maxdep/10.0,threshold):
+				localdep+=[genomeposition[i]]
+				if genomeposition[i]>maxdep:
+					maxdep=genomeposition[i]
+			else:
+				inblock=False
+				end=i
+				if maxdep<=maxdepth:
+					try:
+						peakpos=localdep.index(maxdep)
+					except:
+						print start,end,maxdep
+						print localdep
+						a=input()
+					peakleftsize=0
+					for i in range(peakpos):
+						if localdep[peakpos-i-1]>=maxdep/10.0:
+							peakleftsize+=1
+						else:
+							break
+					svregion+=[(start+peakpos-peakleftsize,end,maxdep)]
+				start=0
+				end=0
+				maxdep=0
+
+		else:
+			if genomeposition[i] > threshold:
+				inblock=True
+				localdep=[genomeposition[i]]
+				start=i
+				maxdep=genomeposition[i]
+
+	svregion=[c for c in svregion if c[2] < maxdepth]
+	allsvinfo={}
+	for c in svregion:
+		allsvinfo[c]=[]
+
+	for c in allsv:
+		start=int(c.split('\t')[1])
+		end=start+int(c.split('\t')[2])
+		for d in svregion:
+			if min(end,d[1])-max(d[0],start)>0:
+				allsvinfo[d]+=[c]
+
+	sv=[]
+	for c in svregion:
+		svinfo=allsvinfo[c]
+		sv+=mergeinfo_insertion(svinfo,mins)
+
+	newsv=[]
+	for c in largedel:
+		testif=0
+		for d in sv:
+			if min(int(c.split('\t')[1])+int(c.split('\t')[2]), int(d.split('\t')[1])+int(d.split('\t')[2])) - max(int(c.split('\t')[1]),int(d.split('\t')[1]))>0 and 0.8*int(d.split('\t')[2])<=int(c.split('\t')[2])<=int(d.split('\t')[2])/0.8:
+				testif=1; break
+		if testif==0:
+			newsv+=[c]
+	newsv+=sv
+	newsv.sort(key=counttimesort)
+
+	t2=time.time()
+	print chrom,t2-t1
+
+	if newsv==[]:
+		return 0
+
+	f=open(outpath+'ae_merge_workspace/del_merged_'+chrom,'w')
+	for c in newsv:
+		f.write(c+'\n')
+	f.close()
+
+
+	return 0
+
+
+
+def cluster_ins(outpath,chrom,contiglength,mins,maxdepth,svtype):
+	t1=time.time()
+        allsv=open(outpath+'debreak_workspace/read_to_contig_'+chrom+'.debreak.temp','r').read().split('\n')[:-1]
+	
+        # Large INS
+	if svtype=='ins':
+	        largesv=[c for c in allsv if 'I-' in c and int(c.split('\t')[2])>2000]
+	else:
+		largesv=[c for c in allsv if 'INV-' in c and int(c.split('\t')[2])>2000]
+
+        window=1600
+        largesv.sort(key=counttimesort)
+        largedel=[]
+        #start=int(largesv[0].split('\t')[1])
+	start=0
+        candi=[]
+        for event in largesv:
+                if int(event.split('\t')[1])<=start+window:
+                        candi+=[event]
+                        continue
+                if len(candi)>=mins:
+                        largedel+=mergeinfo_insertion(candi,mins)
+                candi=[event]
+                start=int(event.split('\t')[1])
+        if len(candi)>=mins:
+                largedel+=mergeinfo_insertion(candi,mins)
+                candi=[]
+	
+	# Small INS
+	if svtype=='ins':
+		allsv=[c for c in allsv if 'I-' in c and int(c.split('\t')[2])<=3000]
+	else:
+		allsv=[c for c in allsv if 'INV-' in c and int(c.split('\t')[2])<=3000]
+
+
+        print len(allsv)
+
+        genomeposition=[0]*contiglength
+
+        for c in allsv:
+                start=int(c.split('\t')[1])-100
+                end=int(c.split('\t')[1])+100
+                original=genomeposition[start-1:end-1]
+                new=[mm+1 for mm in original]
+                genomeposition[start-1:end-1]=new
+	
+	svregion=[]
+        inblock=False
+        threshold=3
+
+        for i in range(len(genomeposition)):
+                if inblock:
+                        if genomeposition[i]>=max(maxdep/10.0,threshold):
+                                localdep+=[genomeposition[i]]
+                                if genomeposition[i]>maxdep:
+                                        maxdep=genomeposition[i]
+                        else:
+                                inblock=False
+                                end=i
+                                if maxdep<=maxdepth:
+                                        try:
+                                                peakpos=localdep.index(maxdep)
+                                        except:
+                                                print start,end,maxdep
+                                                print localdep
+                                                a=input()
+                                        peakleftsize=0
+                                        for i in range(peakpos):
+                                                if localdep[peakpos-i-1]>=maxdep/10.0:
+                                                        peakleftsize+=1
+                                                else:
+                                                        break
+                                        svregion+=[(start+peakpos-peakleftsize,end,maxdep)]
+                                start=0
+                                end=0
+                                maxdep=0
+
+                else:
+                        if genomeposition[i] > threshold:
+                                inblock=True
+                                localdep=[genomeposition[i]]
+                                start=i
+                                maxdep=genomeposition[i]
+
+
+	svregion=[c for c in svregion if c[2] < maxdepth]
+        allsvinfo={}
+        for c in svregion:
+                allsvinfo[c]=[]
+
+        for c in allsv:
+                start=int(c.split('\t')[1])-50
+                end=start+100
+                for d in svregion:
+                        if min(end,d[1])-max(d[0],start)>0:
+                                allsvinfo[d]+=[c]
+
+        sv=[]
+        for c in svregion:
+                svinfo=allsvinfo[c]
+                mergedins=mergeinfo_insertion(svinfo,mins)
+		for m in mergedins:
+			sv+=[m+'\t'+chrom+'\t'+str(c[0])+'\t'+str(c[1])+'\t'+str(c[2])]
+        #sv=[c for c in sv if int(c.split('\t')[3])>=mins]
+
+        newsv=[]
+        for c in largedel:
+                testif=0
+                for d in sv:
+                        if min(int(c.split('\t')[1])+int(c.split('\t')[2]), int(d.split('\t')[1])+int(d.split('\t')[2])) - max(int(c.split('\t')[1]),int(d.split('\t')[1]))>0 and 0.8*int(d.split('\t')[2])<=int(c.split('\t')[2])<=int(d.split('\t')[2])/0.8:
+                                testif=1; break
+                if testif==0:
+                        newsv+=[c]
+        newsv+=sv
+        newsv.sort(key=counttimesort)
+
+        t2=time.time()
+        print chrom,t2-t1
+
+	if newsv==[]:
+		return 0
+
+	if svtype=='ins':
+	        f=open(outpath+'ae_merge_workspace/ins_merged_'+chrom,'w')
+	else:
+		f=open(outpath+'ae_merge_workspace/inv_merged_'+chrom,'w')
+
+        for c in newsv:
+                f.write(c+'\n')
+        f.close()
+
+
+
+	return 0
+
+
+def genotype(depth,outpath):
+	highcov=depth*2
+        allae=open(outpath+'assembly_errors.bed','r').read().split('\n')[:-1]
+
+        samfile=pysam.AlignmentFile(outpath+'read_to_contig.bam',"rb")
+        f=open(outpath+'assembly_errors.bed-gt','w')
+        coll=0
+        expan=0
+        inv=0
+
+        for c in allae:
+                chrom=c.split('\t')[0]
+                start=int(c.split('\t')[1])
+                stop=int(c.split('\t')[2])
+                #print (c)
+                if start<0:
+                        continue
+                if 'Expansion' in c or 'Inversion' in c:
+                        leftcov=samfile.count(chrom,max(start-100,0),start,read_callback='all')
+                        rightcov=samfile.count(chrom,stop,stop+100,read_callback='all')
+                        #if leftcov>highcov and rightcov>highcov:
+                #               continue
+                if 'Collapse' in c:
+                        leftcov=samfile.count(chrom,max(0,start-100),start,read_callback='all')
+                        rightcov=samfile.count(chrom,stop,stop+100,read_callback='all')
+                #       if leftcov>highcov and rightcov>highcov:
+                #               continue
+                gtinfo='./.'
+                if int(c.split('\t')[3])>=0.6*min(leftcov,rightcov):
+                        gtinfo='1/1'
+                else:
+                        gtinfo='1/0'
+
+                f.write(c+'\t'+gtinfo+'\t'+str(leftcov)+'\t'+str(rightcov)+'\t'+str(min(rightcov,leftcov))+'\n')
+        f.close()
+
+
+
+def filterae(depth,outpath,min_size,datatype):
+	allsv=open(outpath+'assembly_errors.bed-gt','r').read().split('\n')[:-1]
+	if datatype=='hifi':
+		rat=0.8
+	else:
+		rat=0.7
+
+	highcov=depth*2
+	lowcov=depth/2
+        exp=[c for c in allsv if 'Exp' in c]
+        col=[c for c in allsv if 'Col' in c]
+        inv=[c for c in allsv if 'Inv' in c]
+
+        new=[]
+        exponly=[]
+        for i in range(len(exp)):
+                c=exp[i].split('\t')
+                testif=0
+                for d in col:
+                        if c[0]==d.split('\t')[0] and int(c[1])-250<=int(d.split('\t')[1])<=250+int(c[2]) and int(c[5].split('=')[1])<20*int(d.split('\t')[5].split('=')[1]):
+                                testif=1
+                                expread=c[6].split(';');goodexp=len(list(dict.fromkeys((expread))))
+                                colread=d.split('\t')[6].split(';'); goodcol=len(list(dict.fromkeys((colread))))
+                                totaln=len(list(dict.fromkeys((expread+colread))))
+                                if 0.33<=int(c[3])/float(d.split('\t')[3])<=3:
+                                        if totaln<min(goodexp+goodcol/2,goodcol+goodexp/2):
+                                                expsize=int(c[5].split('=')[1])
+                                                colsize=int(d.split('\t')[5].split('=')[1])
+                                                if expsize>colsize+min_size:
+                                                        new+=[c[0]+'\t'+c[1]+'\t'+c[2]+'\t'+str(goodexp)+'\t'+c[4]+'\tSize='+str(expsize-colsize)+'\t'+c[7]+'\t'+c[8]+'\t'+c[9]+'\t'+c[10]+'\t'+';'.join(expread)]
+                                                if expsize<colsize-min_size:
+                                                        dd=d.split('\t')
+                                                        new+=[c[0]+'\t'+dd[1]+'\t'+dd[2]+'\t'+str(goodcol)+'\tCollapse\tSize='+str(colsize-expsize)+'\t'+dd[7]+'\t'+dd[8]+'\t'+dd[9]+'\t'+dd[10]+'\t'+';'.join(colread)]
+                                                col.remove(d);break
+                                        else:
+						new+=[c[0]+'\t'+c[1]+';'+d.split('\t')[1]+'\t'+c[2]+';'+d.split('\t')[2]+'\t'+str(totaln)+'\tHeterozygosisError\tSize='+str(int(c[2])-int(c[1]))+';'+d.split('\t')[5].split('=')[1]+'\t-/-\t'+c[8]+'\t'+c[9]+'\t'+c[10]+'\t'+';'.join(expread)+':'+';'.join(colread)+'\t'+str(goodexp)+';'+str(goodcol)]
+				if 0.33>int(c[3])/float(d.split('\t')[3]):
+                                        dd=d.split('\t')
+                                        new+=[c[0]+'\t'+dd[1]+'\t'+dd[2]+'\t'+str(goodcol)+'\tCollapse\tSize='+str(int(c[2])-int(c[1]))+';'+d.split('\t')[5].split('=')[1]+'\t-/-\t'+dd[8]+'\t'+dd[9]+'\t'+dd[10]+'\t'+';'.join(colread)]
+                                if int(c[3])/float(d.split('\t')[3])>3:
+                                        new+=[c[0]+'\t'+c[1]+'\t'+c[2]+'\t'+str(goodexp)+'\tExpansion\tSize='+str(int(c[2])-int(c[1]))+';'+d.split('\t')[5].split('=')[1]+'\t-/-\t'+c[8]+'\t'+c[9]+'\t'+c[10]+'\t'+';'.join(expread)]
+
+                                col.remove(d);break
+
+                if testif==0:
+                        exponly+=[exp[i]]
+	allsv=new
+        for c in exponly+col:
+                c=c.split('\t')
+                expread=c[6].split(';');goodexp=len(list(dict.fromkeys((expread))))
+                allsv+=[c[0]+'\t'+c[1]+'\t'+c[2]+'\t'+str(goodexp)+'\t'+c[4]+'\t'+c[5]+'\t'+c[7]+'\t'+c[8]+'\t'+c[9]+'\t'+c[10]+'\t'+';'.join(expread)]
+	
+        for c in inv:
+                c=c.split('\t')
+                expread=c[5].split(';');goodexp=len(list(dict.fromkeys((expread))))
+                allsv+=[c[0]+'\t'+c[1]+'\t'+c[2]+'\t'+str(goodexp)+'\t'+c[4]+'\tSize='+str(int(c[2])-int(c[1]))+'\t'+c[6]+'\t'+c[7]+'\t'+c[8]+'\t'+c[9]+'\t'+';'.join(expread)]
+
+        new=[]
+
+        for c in allsv:
+                if  max([int(mm) for mm in c.split('\t')[5].split('=')[1].split(';')])<min_size:
+                        continue
+                if int(c.split('\t')[3]) >=10 and int(c.split('\t')[3])>=rat*int(c.split('\t')[9]) and lowcov<=int(c.split('\t')[9])<highcov:
+                        new+=[c]; continue
+		
+	'''
+                if int(c.split('\t')[3]) >=10 and int(c.split('\t')[3])>=(rat-0.2)*int(c.split('\t')[9]) and lowcov<=int(c.split('\t')[9])<highcov and max([int(m) for m in c.split('\t')[5].split('=')[1].split(';')])>=1000:
+                        new+=[c]
+	'''
+        f=open(outpath+'assembly_errors.bed-gt_filtered_newtest','w')
+        for c in new:
+                f.write(c+'\n')
+        f.close()
+
+
+	exp=len([mm for mm in new if 'Exp' in mm])
+	col=len([mm for mm in new if 'Col' in mm])
+	het=len([mm for mm in new if 'Hete' in mm])
+	inv=len([mm for mm in new if 'Inv' in mm])
+	f=open(outpath+'summary_statistics','a')
+	f.write('\n\nStructural error: '+str(len(new))+'\nExpansion: '+str(exp)+'\nCollapse: '+str(col))
+	f.write('\nHeterozygosis error: '+str(het)+'\nInversion: '+str(inv)+'\n')
+	f.close()
+
+	totalbase=0
+	for c in new:
+		if 'Inv' in c:
+			totalbase+=200;continue
+		size=min([int(mm) for mm in c.split('\t')[5].split('=')[1].split(';')]+[10000])
+		totalbase+=size
+	
+        return totalbase
+
+
+
+if __name__ =="__main__":
+	#t=sys.argv[1]
+	#ty=sys.argv[2]
+
+	filterae(79.0337,'/data/scratch/maggic/Inspector_results/hg002_error_evaluation_before_two/canu_ccs/',45,'hifi')
+	
+	quit()
+
+	for t in ['canu','flye','wtpoa']:
+		for ty in ['10','20','30','40','50','60']:
+	#for t in ['dip','hap']:
+#		for ty in ['clr']:
+			path='/data/scratch/maggic/Inspector_results/hg002_clr_downsample/'+t+'_'+ty+'/'
+			#path='/data/scratch/maggic/Inspector_results/sim_contig/'+t+'_'+ty+'/'
+			try:
+				depth=open(path+'summary_statistics','r').read().split('\n')
+			except:
+				continue
+			print t,ty
+			depth=float([c for c in depth if 'Depth:' in c][0].split('\t')[1])
+			filterae(depth,path,45,'clr')
+	quit()
+
+	t='wtpoa'
+	ty='ccs_preset'
+	t=sys.argv[1]
+	ty=sys.argv[2]
+
+	outpath='/data/scratch/maggic/Inspector_results/simulation_'+'ccs'+'/'+t+'_'+ty+'/'
+	alllen=open(outpath+'contig_length_info','r').read().split('\n')[:-1]
+
+	for c in alllen:
+		if int(c.split('\t')[1])>1000000:
+			cluster_ins(outpath,c.split('\t')[0],int(c.split('\t')[1]),7,100,t,ty)
+			cluster(outpath,c.split('\t')[0],int(c.split('\t')[1]),7,100,t,ty)
+
+	ttt=time.time()
+	print 'Total Time  ',ttt-tt
